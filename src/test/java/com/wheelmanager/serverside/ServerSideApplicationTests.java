37dfff4d8fea1761c1de697ca919ebbb664365c5
Hugo Arauco <55633259+Hugo-AP@users.noreply.github.com>
2022-10-19 16:32:14 -0500

    Create Project

---
+++ src/test/java/com/wheelmanager/serverside/ServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheelmanager.serverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSideApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
