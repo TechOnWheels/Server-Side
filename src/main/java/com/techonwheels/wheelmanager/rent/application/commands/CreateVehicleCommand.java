18db717f254cbc9e1c5582bf724a9f3890ba8c55
samirsoft-ux <italo.silva.silvera@outlook.com>
2022-11-01 21:24:19 -0500

    feat: added new bounded context

---
+++ src/main/java/com/techonwheels/wheelmanager/rent/application/commands/CreateVehicleCommand.java
@@ -0,0 +1,21 @@
+package com.techonwheels.wheelmanager.rent.application.commands;
+
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+@Builder
+public class CreateVehicleCommand {
+    @TargetAggregateIdentifier
+    private String vehicleId;
+    private String vehicleName;
+    private String imageURL;
+    private String description;
+    private Double price;
+    //Value Objects
+    private String status;
+    private String brand;
+    private String type;
+    private String ownerId;
+}
