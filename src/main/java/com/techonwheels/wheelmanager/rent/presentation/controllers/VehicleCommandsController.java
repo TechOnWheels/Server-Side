7b76437a4ebc7509e773e0e8a42296306bd514b4
samirsoft-ux <italo.silva.silvera@outlook.com>
2022-11-01 21:31:31 -0500

    feat: develop controllers for rent api

---
+++ src/main/java/com/techonwheels/wheelmanager/rent/presentation/controllers/VehicleCommandsController.java
@@ -0,0 +1,41 @@
+package com.techonwheels.wheelmanager.rent.presentation.controllers;
+
+import com.techonwheels.wheelmanager.rent.application.commands.CreateVehicleCommand;
+import com.techonwheels.wheelmanager.rent.domain.dto.request.VehicleRequest;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/vehicles")
+public class VehicleCommandsController {
+    
+    private final CommandGateway commandGateway;
+    
+    public VehicleCommandsController(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+    
+    @PostMapping
+    public String addVehicle(@RequestBody VehicleRequest vehicleRequest) {
+        CreateVehicleCommand createVehicleCommand =
+                CreateVehicleCommand
+                        .builder()
+                        .vehicleId(UUID.randomUUID().toString())
+                        .vehicleName(vehicleRequest.getVehicleName())
+                        .imageURL(vehicleRequest.getImageURL())
+                        .description(vehicleRequest.getDescription())
+                        .price(vehicleRequest.getPrice())
+                        .status(vehicleRequest.getStatus())
+                        .brand(vehicleRequest.getBrand())
+                        .type(vehicleRequest.getType())
+                        .ownerId(vehicleRequest.getOwnerId())
+                        .build();
+        String result = commandGateway.sendAndWait(createVehicleCommand);
+        return result;
+    }
+}
